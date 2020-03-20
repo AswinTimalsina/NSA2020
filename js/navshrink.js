fd9222fa15dc346eade87b1582a5f31138afbaaa
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-20 17:47:51 -0500

    done with navbar

--- js/navshrink.js
+++ js/navshrink.js
@@ -1,36 +1,11 @@
-// $(window).scroll(function(){
-//     if($(document).scrollTop()>10){
-//         $('nav').addClass('shrink');
-//     }
-//     else{
-//         $('nav').removeClass('shrink');
-//     }
-// });
-
-
-// //or
-
-// // const nav = document.querySelector('nav');
-
-// // window.addEventListener('scroll', function(){
-// //     const offset = window.pageYOffset;
-
-// //     if(offset>75){
-// //         nav.classList.add('scroll');
-// //     }
-// //     else{
-// //         nav.classList.remove('scroll');
-// //     }
-// // });
-
 $(document).on("scroll", function(){
     if($(document).scrollTop() > 100){
-        $("nav").addClass("shrink"); 
-        $(".middle li a").addClass("shrink-black");
+        $("nav").addClass("expand"); 
+        $("img.log").addClass("log-expand");
     }
     else{
-        $("nav").removeClass("shrink");
-        $(".middle li a").removeClass("shrink-black");
+        $("nav").removeClass("expand");
+        $("img.log").removeClass("log-expand");
     }
 
 })
